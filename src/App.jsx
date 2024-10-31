b207544e509776090441711cf23bedddd45bdbe1
Ikkyuz <djdjv999@gmail.com>
2024-10-31 22:11:57 +0700

    Frist file

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  let [counter, setCounter] = useState(5);
+  const update = () => {
+    setCounter(counter+1);
+  }
+
+  const decrease = () => {
+    setCounter(counter-1);
+  }
+
+  return (
+    <div>
+      <h1>หม่าล่าสยองกึ่ม</h1>
+      <h2>
+        ราคาไม้ละ {counter} บาท
+        <button onClick={update}>+</button>
+        <button onClick={decrease}>-</button>
+      </h2>
+      
+      <p>5 ไม้ {counter*5} บาท</p>
+      <p>10 ไม้ {counter*10} บาท</p>
+      <p>20 ไม้ {counter*20} บาท</p>
+      <p>30 ไม้ {counter*30} บาท</p>
+    </div>
+  )
+}
+
+export default App
